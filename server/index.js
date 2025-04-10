5c601ee19ccc8d41d4b36609da75e8fd0df68777
victoramaral22 <victortavamaral@gmail.com>
2025-04-09 23:35:47 -0300

    first commit

---
+++ server/index.js
@@ -0,0 +1,8 @@
+const WebSocket = require('ws');
+const server = new WebSocket.Server({ port: 8080 });
+
+server.on('connection', (ws) => {
+    ws.on('message', (message) => {
+        ws.send(`Server received: ${message}`);
+    })
+});
